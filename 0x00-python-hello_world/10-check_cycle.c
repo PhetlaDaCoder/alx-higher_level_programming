6ae77899161a35524555d4b58ed7760d298787ac
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-09 13:11:38 -0700

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * cycle_check - A function that checks if a singly-linked list has a cycle.
+ * check_cycle - A function that checks if a singly-linked list has a cycle.
  * @list: Pointer to singly-linked list.
  *
  * Return: 1 if success or 0 in failed.
  */
 
-int cycle_check(listint_t *list)
+int check_cycle(listint_t *list)
 {
 	listint_t *slow, *quick;
 
