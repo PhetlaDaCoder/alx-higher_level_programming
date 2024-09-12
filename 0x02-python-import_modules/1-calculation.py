1c4a8e7f5623993406ea584753f1484e6f773ef2
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-12 07:28:51 -0700

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} // {:d} = {:d}".format(a, b, div(a, b)))
+    results = [
+        "{:d} = {:d} = {:d}".format(a, b, add(a, b)),
+        "{:d} - {:d} = {:d}".format(a, b, sub(a, b)),
+        "{:d} * {:d} = {:d}".format(a, b, mul(a, b)),
+        "{:d} / {:d} = {:d}".format(a, b, div(a, b))
+    ]
+
+    print("\n".join(results))
