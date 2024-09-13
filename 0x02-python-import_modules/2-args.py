d86536d13f274ee1bf9ac4cdd186380dac3f2c92
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-13 01:05:08 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+
 def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
-        print("{:d} argument.".format(n))
-        return
+        print("0 arguments.".format(n))
+    elif n == 1:
+        print("1 arguments:".format(n))
     else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+        print("{:d} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{:d}: {:s}".format(i, argv[i]))
 
 if __name__ == "__main__":
     import sys
