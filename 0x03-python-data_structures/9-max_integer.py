53bc2b5b8c2ea00bca6a037f8e9d51241d2f711f
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-16 15:31:17 -0700

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,5 +6,5 @@ def max_integer(my_list=[]):
         total = my_list[0]
         for a in range(len(my_list)):
             if my_list[a] > total:
-                total - my_list[a]
+                total = my_list[a]
             return total
