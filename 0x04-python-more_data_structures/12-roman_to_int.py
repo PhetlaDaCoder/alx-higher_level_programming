0a0293a894ef4e2ed6ab1ad003692997c0c23ddd
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-18 07:59:14 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
-    roman_number= [
+    roman_number = [
         ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
         ['X', 10], ['V', 5], ['I', 1]
     ]
