fca09131b8da73c3990b5e1383a941a59064cf1a
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-18 05:15:12 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for element in my_list:
         if element == search:
-            new_list.appemd(replace)
+            new_list.append(replace)
         else:
             new_list.append(element)
     return new_list
