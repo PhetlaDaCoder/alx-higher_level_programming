0c12beed0e103b97440963ae8bd614c7e11f4344
PhetlaDaCoder <hsphetla@gmail.com>
2024-09-30 15:32:38 -0700

    task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         data = a / b
         print("Inside result: {:.1f}".format(data))
-    except:
+    except (TypeError, ZeroDivisionError):
         data = None
         print("Inside result: {}".format(data))
     finally:
