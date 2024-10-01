2fbf4d0e22b2b954605bb552fd2d2301e9830c0e
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-01 15:06:34 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Arguments:
+        size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size not an interger")
+        elif size < 0:
+            raise ValueError(" Size must be >= 0")
+        self.__size = size
