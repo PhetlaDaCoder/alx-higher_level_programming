b541ccdc2eeb47dca19d5bc183db13b7a1953f20
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-01 15:11:44 -0700

    Task 2 bug fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
         size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError("Size not an interger")
+            raise TypeError("size must be an interger")
         elif size < 0:
-            raise ValueError(" Size must be >= 0")
-        self.__size = sizee
+            raise ValueError("size must be >= 0")
+        self.__size = size
