cd4237c713c2487cc5f8de5ec189dc544a79f2f4
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-07 16:06:28 -0700

    Bug fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represnts a rectangle."""
 
-    def __init__(self, width = 0, height=0):
+    def __init__(self, width=0, height=0):
         """Initializes a new Rectangle.
 
         Arguments:
