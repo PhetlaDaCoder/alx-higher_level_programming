0925a8ea9d1ef5fd5214f4abbe8800fb4878ce9b
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-07 17:25:32 -0700

    Task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, in):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
