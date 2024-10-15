4d84539765d767bb335932159106b4f185bfb504
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-15 13:04:07 -0700

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines class base """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""Defines a Square class"""
+
+
+class Square(Rectangle):
+    """Square Class"""
+    def __init__(self, size):
+        """size init"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
