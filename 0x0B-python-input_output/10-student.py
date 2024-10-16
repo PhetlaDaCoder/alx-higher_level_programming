3eef16466d6ad1aaa99bf66f09828d7cae8a48f4
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-16 13:04:54 -0700

    ws

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -28,7 +28,5 @@ class Student:
         """
         if (isinstance(attrs, list) and
                 all(isinstance(ele, str) for ele in attrs)):
-            return {k: getattr(self, k)
-                    for k in attrs
-                    if hasattr(self, k)}
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
             return self.__dict__
