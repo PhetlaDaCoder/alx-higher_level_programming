e02a19fd259eb400ba84bc5972b07442978d451b
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-16 08:38:55 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,7 +11,8 @@ class Student:
         """Method __init__
         """
         self.first_name = first_name
-        self.last_name = last_name\self.age = age
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Method to_json
