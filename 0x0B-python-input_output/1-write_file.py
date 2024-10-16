de5372db9c3c802406302e1f52210055ba6507d7
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-16 07:03:40 -0700

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a function that writes a string to text file and returns total characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """The module for write_file
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
