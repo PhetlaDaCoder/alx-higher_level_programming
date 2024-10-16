dccc5416dec2e4c59b467f56743c824376992105
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-16 13:47:08 -0700

    sde

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,6 +11,7 @@ class Student:
         Args:
             first_name (str): TYe first name of the student.
             last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
