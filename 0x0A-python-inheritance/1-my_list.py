aef27538c3adb75380e519f2915237d05d4ae1c3
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-17 03:11:46 -0700

    Task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,6 @@ MyList class function
 class MyList(list):
     """A subclass of list that can print itself sorted."""
 
-    def print_sorted(self):    
+    def print_sorted(self):
         """Prints a sorted version on the list"""
         print(sorted(self))
