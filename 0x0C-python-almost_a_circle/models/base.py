1eaa317569d8426fb34c95b939a7a0a41208602b
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-21 23:22:42 -0700

    fixes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -145,7 +145,7 @@ class Base:
                               for d in list_dicts]
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
-                return []
+            return []
 
     @staticmethod
     def draw(list_rectangles, list_squares):
