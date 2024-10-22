09d68c1041ebbb26c4ebd93acc838530a0f4340f
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-22 03:14:13 -0700

    review

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -141,7 +141,8 @@ class Base:
                 else:
                     fieldnames = ["id", "size", "x", "y"]
                 list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
-                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                list_dicts = [dict((k, int(v) if v.isdigit() else v)
+                              for k, v in d.items())
                               for d in list_dicts]
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
