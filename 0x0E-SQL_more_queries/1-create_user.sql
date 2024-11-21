b84ec88677f6dfb6d25637a6f660e528ce30dcad
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-21 14:23:06 -0800

    Tasks

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- creates the MySQL server user user_0d_1.
+
+SELECTtv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_shows_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
