fc4847cdc9ddd90e551ff3d5b89851ce3797bd57
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-21 05:02:29 -0800

    3-list_tables.sql update

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,4 @@
---Lists all tables of a database on MySQL server.
-SHOW  FULL TABLES;
+-- Lists all tables of a database on MySQL server.
+-- Database name will be passed as argument of mysql command
+
+SHOW TABLES;
