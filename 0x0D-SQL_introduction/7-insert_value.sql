29957c61964f14073e8deb5b1e173ce2ad0a32e0
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-23 21:56:41 -0800

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
 -- Inserts a new row into the table first_table in MySQL server.
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
 
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
-
+-- Counts the rowsin first_table.
 SELECT COUNT(*) FROM first_table WHERE id=89;
