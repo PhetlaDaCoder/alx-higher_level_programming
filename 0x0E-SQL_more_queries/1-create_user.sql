191277b9fb94d04767dcb6125756b471c0aed0a2
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-24 14:55:33 -0800

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates the MySQL server user user_0d_1.
 
-SELECTtv_shows.title, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_shows_ratings
 ON tv_shows.id = tv_show_ratings.show_id
