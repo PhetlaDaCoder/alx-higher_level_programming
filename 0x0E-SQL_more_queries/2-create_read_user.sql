78e6348aaa49ae4814743e4e5fac94890d6b1459
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-24 16:17:21 -0800

    bug fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,6 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates user.
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_0d_2pwd";
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2pwd';
+-- grants privileges.
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
