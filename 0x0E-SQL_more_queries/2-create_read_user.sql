4e161e9dafa063b8b1a6b23828191b5c9a907c8d
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-24 16:39:51 -0800

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_2 and user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates user.
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grants privileges.
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
