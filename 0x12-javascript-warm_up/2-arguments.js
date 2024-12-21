d77be30f895d817fc3ce538cbfb5b820d0dff94f
PhetlaDaCoder <hsphetla@gmail.com>
2024-12-20 16:03:16 -0800

    Bug fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const arg = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if (process.arg.length === 2) {
+if (process.args.length === 2) {
 	console.log('No argument');
-} else if (process.arg.length === 3) {
+} else if (process.args.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
