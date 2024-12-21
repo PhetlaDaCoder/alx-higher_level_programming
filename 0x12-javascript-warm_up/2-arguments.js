afab579cf29f2e28f0f91b1ce1dc83b33f4d2183
PhetlaDaCoder <hsphetla@gmail.com>
2024-12-20 16:00:49 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+const arg = process.argv.slice(2);
+
+if (process.arg.length === 2) {
 	console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (process.arg.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
