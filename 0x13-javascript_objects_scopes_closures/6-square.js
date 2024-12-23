9045d77dc837407f12c1edc8422e809abb1bbeed
PhetlaDaCoder <hsphetla@gmail.com>
2024-12-22 21:59:32 -0800

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class Square extends SquareP {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      const s = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
 	      s += c;
       }
