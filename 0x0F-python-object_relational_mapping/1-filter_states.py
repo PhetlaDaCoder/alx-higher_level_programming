f7a7db748853f550af89a9a710c34820428b3889
PhetlaDaCoder <hsphetla@gmail.com>
2025-01-19 04:18:01 -0800

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,13 @@
-#!#!/usr/bin/python3
+#!/usr/bin/python3
 """Lists the states"""
 
 import MySQLdb
 from sys import argv
 
 
-if__name__ == "__main__":
-    conn = MySQL.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
