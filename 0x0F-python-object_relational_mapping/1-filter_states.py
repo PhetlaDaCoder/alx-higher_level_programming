685e9a47d5ab6e588e7345213a11bd86a642e488
PhetlaDaCoder <hsphetla@gmail.com>
2025-01-19 02:42:37 -0800

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!#!/usr/bin/python3
+"""Lists the states"""
+
+import MySQLdb
+from sys import argv
+
+
+if__name__ == "__main__":
+    conn = MySQL.connect(host="localhost";, port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswitch("N"):
+            print(row)
+    cur.close()
+    conn.close()
